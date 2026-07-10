16072564a8a0c5598247f458277946364305493a
jtarra <jonahtarrant28@gmail.com>
2026-07-09 17:37:04 -1000

    Add files via upload

---
+++ maps/colorado/config.js
@@ -0,0 +1,16 @@
+// Hawaii map configuration.
+// Loaded by shared/map-loader.js before shared/map-engine.js.
+
+const CONFIG = {
+      sheetId: '1XBG0uONiQoPvUxqz6V4BJBsOckDM2cahH4WylqOdz74',
+      apiKey: 'AIzaSyDiUAu4Uii6gAltbyJ8G2UkLMUdydyd9Jw',
+      ranges: ['Places!A:L','Reviews!A:E'],
+      mapCenter: [21.4559, -157.9519],
+      mapZoom: 10,
+      attribution: '<a href="https://www.youtube.com/watch?v=rzLIUgnKY40">Home</a>',
+      baseColors: ['#FF6347','#D62828','#00A693','#4D7D5C','#528ED1','#DEFF8D','#32CD32','#FF69B4'],
+      emojiList: ['🤙🏼','🧜🏼‍♀️','🏄🏼‍♂️','🏄🏻‍♀️','🐓','🐋','🐠','🦈','🪸','🪼','🌺','🥥','🍍','🍧','🤿','🌋','🏝️','🛵','👙','🌴','⛱️'],
+      userIconUrls: ['MapResources/UserIcons/Hawaii/Hula1.png','MapResources/UserIcons/Hawaii/Hula2.png','MapResources/UserIcons/Hawaii/Hula3.png','MapResources/UserIcons/Hawaii/Hula4.png','MapResources/UserIcons/Hawaii/Hula5.png']
+};
+const SHEETS_ENDPOINT = 'https://script.google.com/macros/s/AKfycbyB-At6gJCbeod2Hcy-6eYrk81nigJ5w9-6tM533oueHMFvkq5rjh6XrZ3Nzu06xJ0/exec';
+const API_SECRET      = 'Fart';
