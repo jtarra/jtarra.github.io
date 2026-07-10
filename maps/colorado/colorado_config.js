70a508320fc8c0207ff73b666dcc07a03e83390d
jtarra <jonahtarrant28@gmail.com>
2026-07-09 18:03:04 -1000

    Update and rename config.js to colorado_config.js

--- maps/colorado/colorado_config.js
+++ maps/colorado/colorado_config.js
@@ -3,14 +3,14 @@
 
 const CONFIG = {
       sheetId: '1XBG0uONiQoPvUxqz6V4BJBsOckDM2cahH4WylqOdz74',
-      apiKey: 'AIzaSyDiUAu4Uii6gAltbyJ8G2UkLMUdydyd9Jw',
+      apiKey: '1iVs8I3dty6TOU7rEYWEJwa3xTDKSMv769DgLQGfv4GA',
       ranges: ['Places!A:L','Reviews!A:E'],
-      mapCenter: [21.4559, -157.9519],
+      mapCenter: [39.0433, -106.0259],
       mapZoom: 10,
       attribution: '<a href="https://www.youtube.com/watch?v=rzLIUgnKY40">Home</a>',
       baseColors: ['#FF6347','#D62828','#00A693','#4D7D5C','#528ED1','#DEFF8D','#32CD32','#FF69B4'],
-      emojiList: ['🤙🏼','🧜🏼‍♀️','🏄🏼‍♂️','🏄🏻‍♀️','🐓','🐋','🐠','🦈','🪸','🪼','🌺','🥥','🍍','🍧','🤿','🌋','🏝️','🛵','👙','🌴','⛱️'],
+      emojiList: ['🫈','🧗🏻‍♀️','🧗🏼','🦝','🫎','🦌','🦬','🐄','🐻','🦔','🦫','🦆','🌲','🌿','☀️','🏔️','🏕️','⛷️','🏞️','🛶'],
       userIconUrls: ['MapResources/UserIcons/Hawaii/Hula1.png','MapResources/UserIcons/Hawaii/Hula2.png','MapResources/UserIcons/Hawaii/Hula3.png','MapResources/UserIcons/Hawaii/Hula4.png','MapResources/UserIcons/Hawaii/Hula5.png']
 };
-const SHEETS_ENDPOINT = 'https://script.google.com/macros/s/AKfycbyB-At6gJCbeod2Hcy-6eYrk81nigJ5w9-6tM533oueHMFvkq5rjh6XrZ3Nzu06xJ0/exec';
+const SHEETS_ENDPOINT = 'https://script.google.com/macros/s/AKfycbwtPTclCSbU9Ryv-HgNKNE6ZG1ugtfzUwRbvspJjGC-1rwahMGmGVYpgUMysd41eA50/exec';
 const API_SECRET      = 'Fart';
