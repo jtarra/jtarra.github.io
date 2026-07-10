c205df6ff5a0c381eab2b75ece20e7aca8d24b59
jtarra <jonahtarrant28@gmail.com>
2026-07-09 22:20:02 -1000

    Update colorado_config.js

--- maps/colorado/colorado_config.js
+++ maps/colorado/colorado_config.js
@@ -6,7 +6,7 @@ const CONFIG = {
       apiKey: 'AIzaSyDiUAu4Uii6gAltbyJ8G2UkLMUdydyd9Jw',
       ranges: ['Places!A:L','Reviews!A:E'],
       mapCenter: [39.0433, -106.0259],
-      mapZoom: 12,
+      mapZoom: 8,
       attribution: '<a href="https://www.youtube.com/watch?v=rzLIUgnKY40">Home</a>',
       baseColors: ['#FF6347','#D62828','#00A693','#4D7D5C','#528ED1','#DEFF8D','#32CD32','#FF69B4'],
       emojiList: ['🫈','🧗🏻‍♀️','🧗🏼','🦝','🫎','🦌','🦬','🐄','🐻','🦔','🦫','🦆','🌲','🌿','☀️','🏔️','🏕️','⛷️','🏞️','🛶'],
