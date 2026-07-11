726a02c70de812c13c3526accc52bba921161f97
jtarra <jonahtarrant28@gmail.com>
2026-07-10 20:07:19 -1000

    Update japan_config.js

--- maps/japan/japan_config.js
+++ maps/japan/japan_config.js
@@ -11,7 +11,7 @@ const CONFIG = {
       mapLabelLanguage: 'en',
       attribution: '<a href="https://www.youtube.com/watch?v=rzLIUgnKY40">Home</a>',
       baseColors: ['#FF6347','#D62828','#00A693','#4D7D5C','#528ED1','#DEFF8D','#32CD32','#FF69B4'],
-      emojiList: ['🤙🏼','🧜🏼‍♀️','🏄🏼‍♂️','🏄🏻‍♀️','🐓','🐋','🐠','🦈','🪸','🪼','🌺','🥥','🍍','🍧','🤿','🌋','🏝️','🛵','👙','🌴','⛱️'],
+      emojiList: ['🦑','💮','🪷','🌊','🍲','🍥','🍣','🍠','🍜','🍙','🍚','🥥','🏔️','🗾','🚄','🚅','🏝️','🚆','🇯🇵','🥷🏻'],
       userIconUrls: ['MapResources/UserIcons/Hawaii/Hula1.png','MapResources/UserIcons/Hawaii/Hula2.png','MapResources/UserIcons/Hawaii/Hula3.png','MapResources/UserIcons/Hawaii/Hula4.png','MapResources/UserIcons/Hawaii/Hula5.png']
 };
 const SHEETS_ENDPOINT = 'https://script.google.com/macros/s/AKfycbyB-At6gJCbeod2Hcy-6eYrk81nigJ5w9-6tM533oueHMFvkq5rjh6XrZ3Nzu06xJ0/exec';
