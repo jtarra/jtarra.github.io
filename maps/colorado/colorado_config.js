984a84048d8674dae2336a59af592c9de614eed0
jtarra <jonahtarrant28@gmail.com>
2026-07-21 23:37:04 -1000

    Update colorado_config.js

--- maps/colorado/colorado_config.js
+++ maps/colorado/colorado_config.js
@@ -4,7 +4,7 @@
 const CONFIG = {
       sheetId: '1iVs8I3dty6TOU7rEYWEJwa3xTDKSMv769DgLQGfv4GA',
       apiKey: 'AIzaSyDiUAu4Uii6gAltbyJ8G2UkLMUdydyd9Jw',
-      ranges: ['Places!A:M','Reviews!A:E'],
+      ranges: ['Places!A:N','Reviews!A:E'],
       mapCenter: [39.0433, -106.0259],
       mapZoom: 8,
       attribution: '<a href="https://www.youtube.com/watch?v=rzLIUgnKY40">Home</a>',
